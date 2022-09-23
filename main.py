78d4e9898c6abd74ab216bb8ce7ae1431d518e04
hugokhan <114250534+hugokhan@users.noreply.github.com>
2022-09-23 17:59:48 +0000

    New methods GetSymbol and GetExchangeRate are introduced

--- main.py
+++ main.py
@@ -1,18 +1,34 @@
 # currency problem
-
+import math
 
 #subroutine for currency exchange
 def Exchange(currency, rate):
     if currency == "Yuan":
-        return (100 * Yuan_rate)
+        return (100 * rate)
     elif currency == "Yen":
-        return (100 * Yen_rate)
+        return (100 * rate)
     elif currency == "Euro":
-        return (100 * Euro_rate)
+        return (100 * rate)
     elif currency == "USD":
-        USD_value = 100 * USD_rate
-        return ("$" + str(USD_value))
+        return (100 * rate)
 
+def GetSymbol(currency):
+    if currency == "Yuan" or currency == "Yen":
+        return "¥"
+    elif currency == "Euro":
+        return "€"
+    elif currency == "USD":
+        return "$"
+    elif currency == "GBP":
+        return "£"
+
+def GetExchangeRate(currency, rate):
+  return (
+          GetSymbol("GBP") + "100 = " +
+          GetSymbol(currency) +
+          " with round method: " + str(round(Exchange(currency, rate), 2)) + 
+          " with math.ceil method: " + str(math.ceil(Exchange(currency, rate)))
+        )
 
 # main programme
 Yuan_rate = 7.86
@@ -20,5 +36,18 @@ Yen_rate = 157.76
 Euro_rate = 1.12
 USD_rate = 1.10
 
-print("£100 =", Exchange("USD", USD_rate))
-print("£100 =", Exchange("USD", USD_rate))
+#print("£100 =", Exchange("USD", USD_rate))
+#print("£100 =", Exchange("Euro", Euro_rate))
+#print("£100 =", Exchange("Yuan", Yuan_rate))
+#print("£100 =", Exchange("Yen", Yen_rate))
+#print("----------------------------------")
+#print("£100 = " + str(Exchange("USD", USD_rate)))
+#print("£100 = " + str(Exchange("Euro", Euro_rate)))
+#print("£100 = " + str(Exchange("Yuan", Yuan_rate)))
+#print("£100 = " + str(Exchange("Yen", Yen_rate)))
+#print("----------------------------------")
+print(GetExchangeRate("USD", USD_rate))
+print(GetExchangeRate("Euro", Euro_rate))
+print(GetExchangeRate("Yuan", Yuan_rate))
+print(GetExchangeRate("Yen", Yen_rate))
+
